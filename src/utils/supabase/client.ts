5bf335573653902a7b87f077772a1af39bb83ad3
진돌 <wlstjrghdud@naver.com>
2025-06-19 22:51:24 +0900

    fix: 브라우저 supabase client 사용방식 수정

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,9 +1,12 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient as createClient } from "@supabase/ssr";
+import { Database } from "./types/supabase";
 
-export function createClient() {
+function createBrowserClient() {
   // Create a supabase client on the browser with project's credentials
-  return createBrowserClient(
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
+
+export const browserSupabaseClient = createBrowserClient();
